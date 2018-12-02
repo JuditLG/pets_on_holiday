45c5e0064a8895ce6cbd6424f90ccd50a3db74c3
Judit <juditgoes@gmail.com>
2018-12-02 10:55:35 +0000

    copied master into judits_branch

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(function() {
+    var header = $(".navbar-default");
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 50) {
+            header.addClass("scrolled");
+        } else {
+            header.removeClass("scrolled");
+        }
+    });
+});
+
+$(document).ready(function(){
+    // Activate carousel
+    $("#myCarousel").carousel();
+
+    // Enable carousel control
+	$(".left").click(function(){
+    	$("#myCarousel").carousel('prev');
+    });
+    $(".right").click(function(){
+    	$("#myCarousel").carousel('next');
+    });
+
+    // Enable carousel indicators
+    $(".slide-one").click(function(){
+    	$("#myCarousel").carousel(0);
+    });
+    $(".slide-two").click(function(){
+    	$("#myCarousel").carousel(1);
+    });
+    $(".slide-three").click(function(){
+      $("#myCarousel").carousel(2);
+      });
+    });
