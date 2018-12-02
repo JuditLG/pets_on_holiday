94cbae50de46704f5103426c226e9a597456d4f8
Judit <juditgoes@gmail.com>
2018-12-02 20:09:50 +0000

    Added contact form

--- js/main.js
+++ js/main.js
@@ -12,25 +12,87 @@ $(function() {
 });
 
 $(document).ready(function(){
-    // Activate carousel
-    $("#myCarousel").carousel();
+  // Activate carousel
+  $("#myCarousel").carousel();
 
-    // Enable carousel control
-	$(".left").click(function(){
-    	$("#myCarousel").carousel('prev');
-    });
-    $(".right").click(function(){
-    	$("#myCarousel").carousel('next');
-    });
+  // Enable carousel control
+  $(".left").click(function(){
+  	$("#myCarousel").carousel('prev');
+  });
+  $(".right").click(function(){
+  	$("#myCarousel").carousel('next');
+  });
 
-    // Enable carousel indicators
-    $(".slide-one").click(function(){
-    	$("#myCarousel").carousel(0);
+  // Enable carousel indicators
+  $(".slide-one").click(function(){
+  	$("#myCarousel").carousel(0);
+  });
+  $(".slide-two").click(function(){
+  	$("#myCarousel").carousel(1);
+  });
+  $(".slide-three").click(function(){
+    $("#myCarousel").carousel(2);
     });
-    $(".slide-two").click(function(){
-    	$("#myCarousel").carousel(1);
-    });
-    $(".slide-three").click(function(){
-      $("#myCarousel").carousel(2);
+  });
+$(function()
+{
+    function after_form_submitted(data)
+    {
+        if(data.result == 'success')
+        {
+            $('form#reused_form').hide();
+            $('#success_message').show();
+            $('#error_message').hide();
+        }
+        else
+        {
+            $('#error_message').append('<ul></ul>');
+
+            jQuery.each(data.errors,function(key,val)
+            {
+                $('#error_message ul').append('<li>'+key+':'+val+'</li>');
+            });
+            $('#success_message').hide();
+            $('#error_message').show();
+
+            //reverse the response on the button
+            $('button[type="button"]', $form).each(function()
+            {
+                $btn = $(this);
+                label = $btn.prop('orig_label');
+                if(label)
+                {
+                    $btn.prop('type','submit' );
+                    $btn.text(label);
+                    $btn.prop('orig_label','');
+                }
+            });
+
+        }//else
+    }
+
+	$('#reused_form').submit(function(e)
+      {
+        e.preventDefault();
+
+        $form = $(this);
+        //show some response on the button
+        $('button[type="submit"]', $form).each(function()
+        {
+            $btn = $(this);
+            $btn.prop('type','button' );
+            $btn.prop('orig_label',$btn.text());
+            $btn.text('Sending ...');
+        });
+
+
+                    $.ajax({
+                type: "POST",
+                url: 'handler.php',
+                data: $form.serialize(),
+                success: after_form_submitted,
+                dataType: 'json'
+            });
+
       });
-    });
+});
